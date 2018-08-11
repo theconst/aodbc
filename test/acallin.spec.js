97ddf0fa14c493aaf31f827b5fc82a7c45ef776e
kko <k.kovalchuk2017@gmail.com>
2018-08-11 21:12:14 +0300

    Add test, fix formating in package.json

---
+++ test/acallin.spec.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('CacheAdapter DataMapper integration tests', function () {
+
+    this.timeout(60000);
+
+    let acallin; //SUT
+
+    beforeEach(function () {
+        acallin = require('acallin');
+    });
+
+    it('should display version', function () {
+        let version;
+        expect(function () {
+            version = acallin.version();
+        }).to.not.throw();
+        expect(version).to.not.be.empty;
+    });
+
+    afterEach(function () {
+     
+    });
+});
+
+
