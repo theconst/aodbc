16b3ba8f7c488d7ac8f42fc7fec3f6eae640dec3
kko <k.kovalchuk2017@gmail.com>
2018-09-15 18:02:12 +0300

    Addd ODBCStatement to module definition

--- src/nc.cc
+++ src/nc.cc
@@ -1,8 +1,11 @@
 #include <nan.h>
+
 #include "ODBCConnection.hh"
+#include "ODBCStatement.hh"
 
 NAN_MODULE_INIT(Init) {
     NC::ODBCConnection::Init(target);
+    NC::ODBCStatement::Init(target);
 }
 
 NODE_MODULE(NC, Init)
