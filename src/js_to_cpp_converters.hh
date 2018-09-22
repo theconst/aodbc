62550e191ad0d6618eb9cc6edfcf2c48a6c7ce8b
kko <k.kovalchuk2017@gmail.com>
2018-09-22 08:07:51 +0300

    Removed java trace

--- src/js_to_cpp_converters.hh
+++ src/js_to_cpp_converters.hh
@@ -16,39 +16,34 @@ using NC::QueryArguments;
 
 
 template<typename T>
-boost::optional<T> convert_js_type_to_cpp(v8::Local<v8::Value> value);
+boost::optional<T> convert_js_type_to_cpp(v8::Local<v8::Value>);
 
 template<>
 boost::optional<nc_string_t>
-        convert_js_type_to_cpp<nc_string_t>(v8::Local<v8::Value> local);
+        convert_js_type_to_cpp(v8::Local<v8::Value>);
 
 template<>
-boost::optional<nc_null_t>
-        convert_js_type_to_cpp<nc_null_t>(v8::Local<v8::Value> local);
+boost::optional<nc_null_t> convert_js_type_to_cpp(v8::Local<v8::Value>);
 
 template<>
-boost::optional<nc_number_t>
-        convert_js_type_to_cpp<nc_number_t>(v8::Local<v8::Value> local);
+boost::optional<nc_number_t> convert_js_type_to_cpp(v8::Local<v8::Value>);
 
 template<>
-boost::optional<nc_long_t>
-        convert_js_type_to_cpp<nc_long_t>(v8::Local<v8::Value> local);
+boost::optional<nc_long_t> convert_js_type_to_cpp(v8::Local<v8::Value>);
 
 template<>
-boost::optional<QueryArguments> convert_js_type_to_cpp<QueryArguments>(
-        v8::Local<v8::Value> local);
+boost::optional<QueryArguments> convert_js_type_to_cpp(v8::Local<v8::Value>);
 
 template<>
-boost::optional<nc_variant_t> convert_js_type_to_cpp<nc_variant_t>(
-        v8::Local<v8::Value> local);
+boost::optional<nc_variant_t> convert_js_type_to_cpp(v8::Local<v8::Value>);
 
 template<>
-boost::optional<std::vector<nc_variant_t>> convert_js_type_to_cpp<
-                std::vector<nc_variant_t>>(v8::Local<v8::Value> local);
+boost::optional<std::vector<nc_variant_t>> convert_js_type_to_cpp(
+        v8::Local<v8::Value>);
 
 template<>
-boost::optional<PreparedStatementArguments> convert_js_type_to_cpp<
-        PreparedStatementArguments>(v8::Local<v8::Value> local);
+boost::optional<PreparedStatementArguments> convert_js_type_to_cpp(
+        v8::Local<v8::Value>);
 
 
 }  // namespace NC
