fe15c2cad830c8091324957ccb96e6c57226f180
kko <k.kovalchuk2017@gmail.com>
2018-09-29 22:42:05 +0300

    Added promisified version of nc

---
+++ test/nc.promisified.js
@@ -0,0 +1,21 @@
+const nc = require("nc");
+const {promisifyAll} = require('bluebird');
+const squel = require("squel");
+
+squel.useFlavour('mysql');
+
+class PromisifiedNC {
+
+    static createConnection(connectionString) {
+        return promisifyAll(new nc.ODBCConnection(connectionString), {
+            suffix: "Promise",
+        });
+    }
+
+    static createQueryBuilder() {
+        return squel;
+    }
+
+}
+
+module.exports = PromisifiedNC;
