54d29e6f3e0cc4c9bc62bd1aa7e106c012956023
kko <k.kovalchuk@gmail.com>
2018-11-18 02:26:36 +0200

    Added resolution of library on multiple platforms

--- index.js
+++ index.js
@@ -3,4 +3,21 @@
  */
 'use strict'
 
-module.exports = require("bindings")("cacheodbc.node");
+// Mappings from CMAKE CMAKE_SYSTEM to node.js platform
+var platformMapping = {
+    'linux': 'Linux',
+    'win32': 'Windows',
+    'darwin': 'Darwin',
+    'freebsd': 'FreeBSD',
+    'sunos': 'SunOS'
+};
+
+// Fallback to process platform to allow building from source
+var platform = platformMapping[process.platform] || process.platform;
+var arch = process.arch;
+var version = process.version;
+var majorVersion = version.split('.')[0]; // v{majorVersion}
+
+var library = ['cacheodbc', platform, arch, majorVersion].join('-');
+
+module.exports = require("bindings")(library);
